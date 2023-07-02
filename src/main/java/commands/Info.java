395cb1f4d99de213fd4ed711c92daa9ec4b4d6cf
Timo <timokeutgen@gmx.de>
2023-07-02 15:14:56 +0200

    Create Info.java

---
+++ src/main/java/commands/Info.java
@@ -0,0 +1,17 @@
+package commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Info extends ListenerAdapter {
+
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent e) {
+
+        if(!e.getName().equals("info")) return;
+        if(e.getGuild() == null || e.getMember() == null) return;
+
+
+
+    }
+
+}
