4d91aaf58f4b4fe7085ffbfdd256d7e196b3c0f3
Timo <timokeutgen@gmx.de>
2023-07-02 19:05:08 +0200

    Update Kayo.java

--- src/main/java/Kayo/Kayo.java
+++ src/main/java/Kayo/Kayo.java
@@ -10,10 +10,13 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Kayo {
 
+    private static JDA jda;
+
     public static void main(String[] args) {
 
-        String token = "MTEyNDc4NTEzMzAwMjY4NjUyNA.G68Bhq.K7-V6t7ks8neHv0RWsxbqBMerIBcGJgVWaDUvM";
-        JDA jda = JDABuilder.createLight(token)
+        // String token = "MTEyNDc4NTEzMzAwMjY4NjUyNA.G68Bhq.K7-V6t7ks8neHv0RWsxbqBMerIBcGJgVWaDUvM"; // Main Bot
+        String token = "MTEyNTA5NTY3MjA2NTA0ODY1Ng.GA75pJ.hxXkEyzi4W0PL80mWBRISRyGV-CtpkgjOSZVLM"; // Test Bot
+        jda = JDABuilder.createLight(token)
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .enableIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
                 .enableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE, CacheFlag.ACTIVITY, CacheFlag.EMOJI, CacheFlag.STICKER)
@@ -21,9 +24,21 @@ public class Kayo {
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .build();
 
+
+        // Events
         jda.addEventListener(new events.Ready());
         jda.addEventListener(new commands.setupStuff());
 
+        // Commands
+        jda.addEventListener(new commands.Info());
+        jda.addEventListener(new commands.addEmoji());
+
+        // Functions
+
+    }
+
+    public static JDA getJda() {
+        return jda;
     }
 
 }
