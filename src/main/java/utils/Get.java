40fa7fd6098a9ac1b9f992211ecf9c7a99567b3b
Timo <timokeutgen@gmx.de>
2023-07-02 19:05:11 +0200

    Update Get.java

--- src/main/java/utils/Get.java
+++ src/main/java/utils/Get.java
@@ -4,13 +4,24 @@ import java.awt.*;
 
 public class Get {
 
-    public static Color getColor() {
+    public static Color embedColor() {
         return Color.decode("#7700ff");
     }
-
-    public static Color getColor(boolean success) {
+    public static Color embedColor(boolean success) {
         if(success) return Color.decode("#00ff00");
         return Color.decode("#ff0000");
+    }
+    public static KUser timo() {
+        return new KUser(Kayo.Kayo.getJda().getUserById("473737542630637569"));
+    }
+    public static String inviteLink() {
+        return "https://discord.com/api/oauth2/authorize?client_id="+Kayo.Kayo.getJda().getSelfUser().getId()+"&permissions=8&scope=bot%20applications.commands";
+    }
+    public static String topggVoteLink() {
+        return "https://top.gg/bot/"+Kayo.Kayo.getJda().getSelfUser().getId()+"/vote";
+    }
+    public static long topggVotes() {
+        return 0;
     }
 
 }
