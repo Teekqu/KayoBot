1b087c437cfa2ca365ea92e5ec0baa36a60f9c2d
Timo <timokeutgen@gmx.de>
2023-07-02 19:04:45 +0200

    Create Emojis.java

---
+++ src/main/java/utils/Emojis.java
@@ -0,0 +1,52 @@
+package utils;
+
+public class Emojis {
+
+    public static String kayoNormal() { return "<:Kayo:1125023200426864640>"; }
+    public static String activities() { return "<:k_activities:1125021039412400168>"; }
+    public static String awardcup() { return "<:k_awardcup:1125021035394244689>"; }
+    public static String archive() { return "<:k_archive:1125021042398740541>"; }
+    public static String ban() { return "<:k_ban:1125021044076445696>"; }
+    public static String bugs() { return "<:k_bugs:1125021036879040564>"; }
+    public static String calendar() { return "<:k_calendar:1125021033603276820>"; }
+    public static String channel() { return "<:k_channel:1125021038120538183>"; }
+    public static String clock() { return "<:k_clock:1125020987373650011>"; }
+    public static String code() { return "<:k_code:1125020988938129509>"; }
+    public static String crone() { return "<:k_crone:1125021032084938792>"; }
+    public static String data() { return "<:k_data:1125020990674567168>"; }
+    public static String delete() { return "<:k_delete:1125020992150970409>"; }
+    public static String download() { return "<:k_download:1125020993623171072>"; }
+    public static String earth() { return "<:k_earth:1125020994797572097>"; }
+    public static String help() { return "<:k_help:1125020995938426892>"; }
+    public static String idee() { return "<:k_idee:1125020997679071283>"; }
+    public static String info() { return "<:k_info:1125020999184826420>"; }
+    public static String news() { return "<:k_news:1125021040788111370>"; }
+    public static String mute() { return "<:k_mute:1125021029245399171>"; }
+    public static String mail() { return "<:k_mail:1125021006516469850>"; }
+    public static String loading() { return "<:k_loading:1125021030767927397>"; }
+    public static String list() { return "<:k_list:1125021005321076808>"; }
+    public static String link() { return "<:k_link:1125021003869855805>"; }
+    public static String leave() { return "<:k_leave:1125021002506711162>"; }
+    public static String kayo() { return "<:k_kayo:1125024213544206436>"; }
+    public static String join() { return "<:k_join:1125021000761876521>"; }
+    public static String no() { return "<:k_no:1125021008257101924>"; }
+    public static String partner() { return "<:k_partner:1125021046450442300>"; }
+    public static String pen() { return "<:k_pen:1125021011025334332>"; }
+    public static String user() { return "<:k_user:1125021020668043295>"; }
+    public static String reply() { return "<:k_reply:1125021026548469880>"; }
+    public static String search() { return "<:k_search:1125021027999678475>"; }
+    public static String settings() { return "<:k_settings:1125021024900100166>"; }
+    public static String share() { return "<:k_share:1125021023457247273>"; }
+    public static String staff() { return "<:k_staff:1125021047721312278>"; }
+    public static String yes() { return "<:k_yes:1125021009876090880>"; }
+    public static String wumpus() { return "<:k_wumpus:1125021012380094595>"; }
+    public static String wave() { return "<:k_wave:1125021013193789442>"; }
+    public static String warning() { return "<:k_warning:1125021014821187645>"; }
+    public static String vote() { return "<:k_vote:1125021015974617128>"; }
+    public static String verified() { return "<:k_verified:1125021045305380874>"; }
+    public static String timeout() { return "<:k_timeout:1125021022123470911>"; }
+    public static String slash() { return "<:k_slash:1125085430371582093>"; }
+    public static String supportman() { return "<:k_supportman:1125021017815912468>"; }
+    public static String stats() { return "<:k_stats:1125021019216810079>"; }
+
+}
