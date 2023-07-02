dcffbaee52dd9e1dec792cfe122d054aee28f418
Timo <timokeutgen@gmx.de>
2023-07-02 19:05:06 +0200

    Update Ready.java

--- src/main/java/events/Ready.java
+++ src/main/java/events/Ready.java
@@ -3,8 +3,10 @@ package events;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import utils.KUser;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -22,12 +24,27 @@ public class Ready extends ListenerAdapter {
         System.out.println("Commands: "+e.getJDA().retrieveCommands().complete().size());
         System.out.println("----- [ SYSTEM ] -----");
 
+        loadCommands();
+
          try{
              ScheduledThreadPoolExecutor thread = new ScheduledThreadPoolExecutor(1);
              thread.scheduleWithFixedDelay(statusTask(e.getJDA()), 0, 1, TimeUnit.MINUTES);
          } catch (Exception err) {
              e.getJDA().getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("einen Bot"));
          }
+
+    }
+
+    private void loadCommands() {
+
+        JDA jda = Kayo.Kayo.getJda();
+        Guild g = jda.getGuildById("820343748244144169");
+
+        g.upsertCommand("info", "Siehe Informationen über den Bot an").setGuildOnly(true).queue();
+        g.upsertCommand("add-emoji", "Füge ein Emoji zum Server hinzu")
+                .addOption(OptionType.STRING, "emoji", "Gebe ein Emoji an", true)
+                .addOption(OptionType.STRING, "name", "Gebe einen Namen für das Emoji an", false)
+                .setGuildOnly(true).queue();
 
     }
 
