e2d921f8b71515eb045fa653657c216175e07282
Timo <timokeutgen@gmx.de>
2023-07-02 19:04:40 +0200

    Create addEmoji.java

---
+++ src/main/java/commands/addEmoji.java
@@ -0,0 +1,59 @@
+package commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Icon;
+import net.dv8tion.jda.api.entities.emoji.CustomEmoji;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.entities.emoji.RichCustomEmoji;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.utils.TimeFormat;
+import utils.*;
+
+public class addEmoji extends ListenerAdapter {
+
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent e) {
+
+        if(!e.getName().equals("add-emoji")) return;
+        if(e.getGuild() == null || e.getMember() == null) return;
+
+        KUser bot = new KUser(e.getJDA().getSelfUser());
+        KUser u = new KUser(e.getUser());
+        KGuild g = new KGuild(e.getGuild());
+
+        if(u.isBlocked()) return;
+
+        InteractionHook ih = e.deferReply(true).complete();
+
+        String emoji1 = e.getOption("emoji").getAsString();
+        CustomEmoji emoji = Emoji.fromFormatted(emoji1).asCustom();
+        if(emoji.getName().equals(emoji.getFormatted())) {
+            ih.editOriginalEmbeds(Embeds.error(g, u, "Ungültiges Emoji")).queue();
+            return;
+        }
+
+        String name = emoji.getName();
+        if(e.getOption("name") != null) name = e.getOption("name").getAsString();
+
+        try {
+            RichCustomEmoji newEmoji = e.getGuild().createEmoji(name, Icon.from(emoji.getImage().download().get())).complete();
+
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle(Emojis.yes() + " │ Erfolgreich!")
+                    .setDescription("Das Emoji "+newEmoji.getAsMention()+" wurde Erfolgreich mit dem Namen **"+newEmoji.getName()+"** zum Server hinzugefügt!")
+                    .setColor(Get.embedColor(true))
+                    .setTimestamp(TimeFormat.RELATIVE.now().toInstant())
+                    .setThumbnail(e.getGuild().getIconUrl())
+                    .setFooter(u.getUsername(), u.getEffectiveAvatarUrl());
+
+            ih.editOriginalEmbeds(embed.build()).queue();
+
+        } catch (Exception err) {
+            ih.editOriginalEmbeds(Embeds.error(g, u, err.getMessage())).queue();
+            return;
+        }
+
+    }
+
+}
