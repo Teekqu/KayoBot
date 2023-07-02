27d80ba752e5980b744cb3a45d56056f079485d8
Timo <timokeutgen@gmx.de>
2023-07-02 15:15:02 +0200

    Create Kayo.java

---
+++ src/main/java/Kayo/Kayo.java
@@ -0,0 +1,29 @@
+package Kayo;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+public class Kayo {
+
+    public static void main(String[] args) {
+
+        String token = "MTEyNDc4NTEzMzAwMjY4NjUyNA.G68Bhq.K7-V6t7ks8neHv0RWsxbqBMerIBcGJgVWaDUvM";
+        JDA jda = JDABuilder.createLight(token)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .enableIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
+                .enableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE, CacheFlag.ACTIVITY, CacheFlag.EMOJI, CacheFlag.STICKER)
+                .setActivity(Activity.playing("booting up"))
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .build();
+
+        jda.addEventListener(new events.Ready());
+        jda.addEventListener(new commands.setupStuff());
+
+    }
+
+}
