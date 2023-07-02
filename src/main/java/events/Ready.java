73eb4c6b97559d2335c2004e58943131e4f3dec5
Timo <timokeutgen@gmx.de>
2023-07-02 15:15:00 +0200

    Create Ready.java

---
+++ src/main/java/events/Ready.java
@@ -0,0 +1,51 @@
+package events;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import utils.KUser;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Ready extends ListenerAdapter {
+
+    public void onReady(ReadyEvent e) {
+
+        KUser u = new KUser(e.getJDA().getSelfUser());
+
+        System.out.println("----- [ SYSTEM ] -----");
+        System.out.println("Bot: "+u.getName()+" ("+u.getUsername()+")");
+        System.out.println("Server: "+e.getJDA().getGuilds().size());
+        System.out.println("Commands: "+e.getJDA().retrieveCommands().complete().size());
+        System.out.println("----- [ SYSTEM ] -----");
+
+         try{
+             ScheduledThreadPoolExecutor thread = new ScheduledThreadPoolExecutor(1);
+             thread.scheduleWithFixedDelay(statusTask(e.getJDA()), 0, 1, TimeUnit.MINUTES);
+         } catch (Exception err) {
+             e.getJDA().getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("einen Bot"));
+         }
+
+    }
+
+    private Runnable statusTask(JDA jda) {
+        return () -> {
+
+            jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("on "+jda.getGuilds().size()+" Servers"));
+            try { Thread.sleep(60000); } catch (Exception ignored) { }
+            jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("the boss"));
+            try { Thread.sleep(60000); } catch (Exception ignored) { }
+            jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("with "+jda.getUsers().size()+" Users"));
+            try { Thread.sleep(60000); } catch (Exception ignored) { }
+            jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("around with rights"));
+            try { Thread.sleep(60000); } catch (Exception ignored) { }
+            jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing("with slash commands"));
+            try { Thread.sleep(60000); } catch (Exception ignored) { }
+
+        };
+    }
+
+}
