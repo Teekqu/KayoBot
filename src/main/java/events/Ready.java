0223f8797a4af24a5d3fb7e349b0a51d75fbff87
Timo <timokeutgen@gmx.de>
2023-07-17 18:23:28 +0200

    Update Ready.java

--- src/main/java/events/Ready.java
+++ src/main/java/events/Ready.java
@@ -45,13 +45,13 @@ public class Ready extends ListenerAdapter {
         if(g == null) return;
 
         /* COMMANDS */
-        g.upsertCommand("info", "Siehe Informationen über den Bot an").setGuildOnly(true).queue();
-        g.upsertCommand("add-emoji", "Füge ein Emoji zum Server hinzu")
+        jda.upsertCommand("info", "Siehe Informationen über den Bot an").setGuildOnly(true).queue();
+        jda.upsertCommand("add-emoji", "Füge ein Emoji zum Server hinzu")
                 .addOption(OptionType.STRING, "emoji", "Gebe ein Emoji an", true)
                 .addOption(OptionType.STRING, "name", "Gebe einen Namen für das Emoji an", false)
                 .setGuildOnly(true).queue();
-        g.upsertCommand("serverinfo", "Siehe Informationen über den Server an").setGuildOnly(true).queue();
-        g.upsertCommand("userinfo", "Siehe Informationen über einen User an")
+        jda.upsertCommand("serverinfo", "Siehe Informationen über den Server an").setGuildOnly(true).queue();
+        jda.upsertCommand("userinfo", "Siehe Informationen über einen User an")
                 .addOption(OptionType.USER, "user", "Wähle einen User", false)
                 .setGuildOnly(true).queue();
 
@@ -67,7 +67,7 @@ public class Ready extends ListenerAdapter {
         SubcommandData createautodeletecmd = new SubcommandData("add", "Füge einen AutoDelete Kanal hinzu")
                 .addOptions(createautodeletedata1, createautodeletedata2, createautodeletedata3);
         SubcommandData showautodeletecmd = new SubcommandData("show", "Siehe alle AutoDelete Channel und bearbeiten diese");
-        g.upsertCommand("autodelete", "Verwalte das AutoDelete System")
+        jda.upsertCommand("autodelete", "Verwalte das AutoDelete System")
                 .addSubcommands(createautodeletecmd, showautodeletecmd)
                 .setGuildOnly(true).queue();
 
@@ -77,21 +77,21 @@ public class Ready extends ListenerAdapter {
         SubcommandData createautoreactcmd = new SubcommandData("add", "Füge AutoReact zu einem Kanal hinzu")
                 .addOptions(createautoreactdata1, createautoreactdata2);
         SubcommandData showautoreactcmd = new SubcommandData("show", "Siehe alle AutoReact Channel und bearbeite diese");
-        g.upsertCommand("autoreact", "Verwalte das AutoReact System")
+        jda.upsertCommand("autoreact", "Verwalte das AutoReact System")
                 .addSubcommands(createautoreactcmd, showautoreactcmd)
                 .setGuildOnly(true).queue();
 
         OptionData createjoinroledata = new OptionData(OptionType.ROLE, "role", "Wähle eine Rolle", true);
         SubcommandData createjoinrolecmd = new SubcommandData("add", "Füge eine JoinRole hinzu").addOptions(createjoinroledata);
-        g.upsertCommand("joinrole", "Verwalte das JoinRole System")
+        jda.upsertCommand("joinrole", "Verwalte das JoinRole System")
                 .addSubcommands(createjoinrolecmd)
                 .setGuildOnly(true).queue();
-        g.upsertCommand("joinroles", "Siehe alle JoinRoles und bearbeite diese")
+        jda.upsertCommand("joinroles", "Siehe alle JoinRoles und bearbeite diese")
                 .setGuildOnly(true).queue();
 
 
         SubcommandData automodrulescmd = new SubcommandData("rules", "Verwalte die AutoMod Regeln");
-        g.upsertCommand("automod", "Verwalte das AutoMod System von Discord").addSubcommands(automodrulescmd).setGuildOnly(true).queue();
+        jda.upsertCommand("automod", "Verwalte das AutoMod System von Discord").addSubcommands(automodrulescmd).setGuildOnly(true).queue();
     }
 
     private Runnable statusTask(JDA jda) {
