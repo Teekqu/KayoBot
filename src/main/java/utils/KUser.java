a0f8221a93f7f7568abfd86b77fb7f9a4e4fda40
Timo <timokeutgen@gmx.de>
2023-07-17 17:20:49 +0200

    Added Topgg Functions to KUser.java

--- src/main/java/utils/KUser.java
+++ src/main/java/utils/KUser.java
@@ -2,6 +2,8 @@ package utils;
 
 import net.dv8tion.jda.api.entities.User;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.util.Collection;
@@ -51,6 +53,57 @@ public class KUser {
     }
     public boolean isSystem() {
         return this.u.isSystem();
+    }
+
+    /* Eigene Funktionen */
+    public long getVoteCount() {
+        Statement stm = MySQL.connect();
+        try {
+            ResultSet rs = stm.executeQuery("SELECT * FROM TopggVotes WHERE userId="+this.getId());
+            if(!rs.next()) {
+                stm.execute("INSERT INTO TopggVotes(userId, count, lastVote) VALUES("+this.getId()+",0,0);");
+                try { stm.close(); } catch (Exception ignored) { }
+                return 0;
+            }
+            long count = Long.parseLong(rs.getString(2));
+            try { stm.close(); } catch (Exception ignored) { }
+            return count;
+        } catch (Exception err) {
+            try { stm.close(); } catch (Exception ignored) { }
+            err.printStackTrace();
+            return -1;
+        }
+    }
+    public long getLastVote() {
+        Statement stm = MySQL.connect();
+        try {
+            ResultSet rs = stm.executeQuery("SELECT * FROM TopggVotes WHERE userId="+this.getId());
+            if(!rs.next()) {
+                stm.execute("INSERT INTO TopggVotes(userId, count, lastVote) VALUES("+this.getId()+",0,0);");
+                try { stm.close(); } catch (Exception ignored) { }
+                return 0;
+            }
+            long time = Long.parseLong(rs.getString(3));
+            try { stm.close(); } catch (Exception ignored) { }
+            return time;
+        } catch (Exception err) {
+            try { stm.close(); } catch (Exception ignored) { }
+            err.printStackTrace();
+            return -1;
+        }
+    }
+    public boolean setVoteCount(Long newCount, Long lastVote) {
+        Statement stm = MySQL.connect();
+        try {
+            long oldCount = this.getVoteCount();
+            stm.execute("UPDATE TopggVotes SET count="+newCount+" AND lastVote="+lastVote+" WHERE userId="+this.getId());
+            try { stm.close(); } catch (Exception ignored) { }
+            return true;
+        } catch (Exception err) {
+            try { stm.close(); } catch (Exception ignored) { }
+            err.printStackTrace();
+            return false;
+        }
     }
 
 }
