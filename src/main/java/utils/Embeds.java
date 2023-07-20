96f660ef764c14feb9ed97237d4c9379e2f6cd4d
Timo <timokeutgen@gmx.de>
2023-07-20 16:43:02 +0200

    Update Embeds.java

--- src/main/java/utils/Embeds.java
+++ src/main/java/utils/Embeds.java
@@ -20,4 +20,18 @@ public class Embeds {
 
     }
 
+    public static MessageEmbed error(KUser user, String error) {
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setTitle(Emojis.no() + " │ Es ist ein Fehler vorgefallen!")
+                .setDescription("**Server:** None\n**User:** "+user.getAsMention()+" [`"+user.getUsername()+"`]\n\r**Fehler:** ```java\n"+error+"```")
+                .setColor(Get.embedColor(false))
+                .setTimestamp(TimeFormat.RELATIVE.now().toInstant())
+                .setThumbnail("https://i.timo1005.de/loading.gif")
+                .setAuthor(Kayo.Kayo.getJda().getSelfUser().getName(), null, Kayo.Kayo.getJda().getSelfUser().getEffectiveAvatarUrl())
+                .setFooter(user.getUsername(), user.getEffectiveAvatarUrl());
+        return embed.build();
+
+    }
+
 }
