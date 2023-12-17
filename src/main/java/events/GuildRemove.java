39a9bc867d2c61465fa894849b9d60f2b62b91b6
Timo <timokeutgen@gmx.de>
2023-12-17 12:14:38 +0100

    Added review system when bot leave guild

--- src/main/java/events/GuildRemove.java
+++ src/main/java/events/GuildRemove.java
@@ -1,12 +1,18 @@
 package events;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
 import net.dv8tion.jda.api.utils.TimeFormat;
 import utils.Emojis;
 import utils.Get;
@@ -63,6 +69,74 @@ public class GuildRemove extends ListenerAdapter {
         });
         g.getTempChannels().forEach(g::removeTempChannel);
         g.getJoinHubs().forEach(g::removeJoinHub);
+
+        sendFeedbackQuestion(g);
+
+    }
+
+    private void sendFeedbackQuestion(KGuild g) {
+
+        User user = Kayo.Kayo.getJda().getUserById(g.getOwnerIdLong());
+        if(user != null) {
+
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle(Emojis.vote()+" | **Wir brauchen deine Meinung!**")
+                    .setDescription("Guten Tag "+ user.getAsMention() +",\n\rwir haben soeben mitbekommen, dass Sie Kayo von Ihrem Server entfernt haben,\ndies kann viele individuelle Gründe haben, die wir alle akzeptieren.\n\rJedoch würden wir uns sehr über Ihre Rückmeldung freuen, wo das Problem lag,\ndamit wir Kayo weiterhin verbessern können, um mehr Leuten Zufriedenheit zu garantieren.\n\r> **Mit freundlichen Grüßen**\n> ~ **"+Emojis.kayo()+" Entwicklungsteam**")
+                    .setColor(Get.embedColor())
+                    .setThumbnail(g.getIconUrl())
+                    .setFooter(null, "https://i.timo1005.de/Kayo.png")
+                    .setTimestamp(TimeFormat.RELATIVE.now().toInstant());
+
+            Button btn = Button.primary("guild.leave.btn.review."+g.getId(), "Bewerten").withEmoji(Emoji.fromFormatted(Emojis.vote()));
+            user.openPrivateChannel().complete().sendMessageEmbeds(embed.build()).setActionRow(btn).queue();
+
+        }
+
+    }
+
+    public void onButtonInteraction(ButtonInteractionEvent e) {
+
+        if(!e.getButton().getId().startsWith("guild.leave.btn.review.")) return;
+
+        TextInput system = TextInput.create("system", "Was am Bot hat dir nicht gefallen?", TextInputStyle.PARAGRAPH)
+                .setPlaceholder("TempChannels, AutoDelete, Logging, ...")
+                .setMinLength(2)
+                .setMaxLength(250)
+                .setRequired(true)
+                .build();
+        TextInput description = TextInput.create("description", "Gebe uns genauere Informationen zum Problem. (Optional)", TextInputStyle.PARAGRAPH)
+                .setPlaceholder("Es gab einen Bug, Mir hat das Design nicht gefallen, ...")
+                .setMinLength(10)
+                .setMaxLength(3000)
+                .setRequired(false)
+                .build();
+        Modal modal = Modal.create("guild.leave.modal."+e.getButton().getId().substring(23), "Was hat dir an Kayo nicht gefallen?")
+                .addActionRow(system)
+                .addActionRow(description)
+                .build();
+
+        e.replyModal(modal).queue();
+
+    }
+
+    public void onModalInteraction(ModalInteractionEvent e) {
+
+        if(!e.getModalId().startsWith("guild.leave.modal.")) return;
+
+        String system = e.getValue("system").getAsString();
+        String description = "*Keine Rückmeldung*";
+        if(e.getValue("description")!=null) description = e.getValue("description").getAsString();
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setTitle(Emojis.vote() + " | **Neue Bewertung**")
+                .setDescription("**User:** "+e.getUser().getName()+"\n**Server:** "+e.getJDA().getGuildById(e.getModalId().split("\\.")[3]).getName()+"\n\r> **Was am Bot hat dir nicht gefallen?**\n"+system+"\n\r> **Gebe uns genauere Informationen zum Problem. (Optional)**\n"+description)
+                .setColor(Get.embedColor())
+                .setTimestamp(TimeFormat.RELATIVE.now().toInstant())
+                .setThumbnail(e.getUser().getEffectiveAvatarUrl())
+                .setFooter("Bewertet über guild.leave.modal", e.getJDA().getSelfUser().getEffectiveAvatarUrl());
+        e.getJDA().getGuildById("1124803921978871850").getTextChannelById("1185900962603347989").sendMessageEmbeds(embed.build()).queue();
+
+        e.reply(Emojis.yes()+" | **Vielen Dank für deine Bewertung!**\n"+Emojis.yes()+" | **Die Bewertung wurde an das Entwicklungsteam weitergeleitet.**").setEphemeral(true).queue();
 
     }
 
